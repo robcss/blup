8c18c9037269169ea3818e17d8b816ff95308a51
robcass <77405130+robcss@users.noreply.github.com>
2021-03-21 18:20:38 +0100

    add database seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,71 @@
-const cities = require("./italy_munic.json")
-const { randomInt } = require("../utils")
+const mongoose = require("mongoose");
+const municipalitiesDataset = require("./italy_munic.json");
+const { randomInt } = require("../utils");
+
+const Fountain = require("../models/fountain");
+
+mongoose.connect('mongodb://localhost:27017/fountain-finder', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 
-const citiesMaxIndex = cities.length - 3 // avoid selecting the last two objects in the array
+const municMaxIndex = municipalitiesDataset.length - 3 // avoid selecting the last two objects in the array
 
 const seedsNumber = 50
 
-for (let i = 0; i < seedsNumber; i++) {
-    const randIndex = randomInt(citiesMaxIndex)
-    const city = cities[randIndex]
-    console.log(city)
+const seedDB = async () => {
+
+    await Fountain.deleteMany({});
+
+    for (let i = 0; i < seedsNumber; i++) {
+
+        const randIndex = randomInt(municMaxIndex)
+
+        const munic = municipalitiesDataset[randIndex]
+
+        const address = {
+            street: munic.indirizzo,
+            city: munic.comune,
+            state: munic.provincia,
+            country: "Italia"
+        }
+
+        const fountain = new Fountain({ address })
+
+        await fountain.save()
+    }
 }
+
+seedDB().then(() => {
+    console.log("DB seeded")
+    mongoose.connection.close();
+})
+
+
+// const seedTest = async () => {
+//     const fountain = new Fountain({
+//         address: {
+//             street: "Via Bari",
+//             // number: 2,
+//             // postcode: 00161,
+//             city: "Roma",
+//             state: "RM",
+//             country: "Italia"
+//         }
+//     })
+
+//     await fountain.save();
+// }
+
+// seedTest().then(() => {
+//     mongoose.connection.close();
+// })
