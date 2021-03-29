6147a26f27534b0d8797786734775e2f95137303
robcass <77405130+robcss@users.noreply.github.com>
2021-03-29 17:54:33 +0200

    add inputFields

--- public/js/formValidation/index.js
+++ public/js/formValidation/index.js
@@ -1,79 +1,7 @@
-import { InputField } from "./InputField.js"
-
-const createInputField = (id) => {
-    return new InputField(id)
-}
+import { inputFields } from "./inputFields.js"
 
 
-const [inputStreet, inputNumber, inputCity, inputCountry] = ["inputStreet", "inputNumber", "inputCity", "inputCountry"].map(id => createInputField(id))
-
-
-inputStreet.validate = function () {
-    if (this.isNotEmpty) {
-        const regex = /^\b[a-zA-Z]+\b\s(\b[a-zA-Z]+\b\s?)+/g; // at least two words(letters only) separated by single spaces
-
-        const match = this.inputElement.value.match(regex)
-
-        const regexTest = match ? match[0] === this.inputElement.value : false //check if all input matches
-
-        if (regexTest) {
-            this.setSuccess("Street is valid")
-            this.validated = true
-        } else {
-            this.setDanger("Invalid street name")
-            this.validated = false
-        }
-    } else {
-        this.setDanger("Field is required")
-        this.validated = false
-    }
-}
-
-inputNumber.validate = function () {
-    if (this.isNotEmpty) {
-
-        const regex = /^\d+$/g
-        const regexTest = regex.test(this.inputElement.value)
-
-        if (regexTest) {
-            this.setSuccess("Number is valid")
-            this.validated = true
-        } else {
-            this.setDanger("Number must be a single integer")
-            this.validated = false
-        }
-    } else {
-        this.setDanger("Field is required")
-        this.validated = false
-    }
-}
-
-inputCity.validate = function () {
-    if (this.isNotEmpty) {
-        const regex = /^([a-zA-Z]+\s{0,1})+/g; // one word(letters only) or more separated by single spaces
-
-        const match = this.inputElement.value.match(regex)
-
-        const regexTest = match ? match[0] === this.inputElement.value : false //check if all input matches
-
-        if (regexTest) {
-            this.setSuccess("City is valid")
-            this.validated = true
-        } else {
-            this.setDanger("Invalid city name")
-            this.validated = false
-        }
-    } else {
-        this.setDanger("Field is required")
-        this.validated = false
-    }
-}
-
-inputCountry.validate = inputCountry.validateEmpty
-
-
-
-// button logic
+// submit button logic
 const sumbitButton = document.getElementById("submitButton")
 
 const disableSubmit = () => {
@@ -100,8 +28,6 @@ const tryActivateSumbit = (inputFields) => {
 }
 
 //validation
-const inputFields = [inputStreet, inputNumber, inputCity, inputCountry]
-
 document.addEventListener('change', event => {
 
     for (let field of inputFields) {
---
+++ public/js/formValidation/inputFields.js
@@ -0,0 +1,76 @@
+import { InputField } from "./InputField.js"
+
+
+const createInputField = (id) => {
+    return new InputField(id)
+}
+
+
+const [inputStreet, inputNumber, inputCity, inputCountry] = ["inputStreet", "inputNumber", "inputCity", "inputCountry"].map(id => createInputField(id))
+
+
+inputStreet.validate = function () {
+    if (this.isNotEmpty) {
+        const regex = /^\b[a-zA-Z]+\b\s(\b[a-zA-Z]+\b\s?)+/g; // at least two words(letters only) separated by single spaces
+
+        const match = this.inputElement.value.match(regex)
+
+        const regexTest = match ? match[0] === this.inputElement.value : false //check if all input matches
+
+        if (regexTest) {
+            this.setSuccess("Street is valid")
+            this.validated = true
+        } else {
+            this.setDanger("Invalid street name")
+            this.validated = false
+        }
+    } else {
+        this.setDanger("Field is required")
+        this.validated = false
+    }
+}
+
+inputNumber.validate = function () {
+    if (this.isNotEmpty) {
+
+        const regex = /^\d+$/g
+        const regexTest = regex.test(this.inputElement.value)
+
+        if (regexTest) {
+            this.setSuccess("Number is valid")
+            this.validated = true
+        } else {
+            this.setDanger("Number must be a single integer")
+            this.validated = false
+        }
+    } else {
+        this.setDanger("Field is required")
+        this.validated = false
+    }
+}
+
+inputCity.validate = function () {
+    if (this.isNotEmpty) {
+        const regex = /^([a-zA-Z]+\s{0,1})+/g; // one word(letters only) or more separated by single spaces
+
+        const match = this.inputElement.value.match(regex)
+
+        const regexTest = match ? match[0] === this.inputElement.value : false //check if all input matches
+
+        if (regexTest) {
+            this.setSuccess("City is valid")
+            this.validated = true
+        } else {
+            this.setDanger("Invalid city name")
+            this.validated = false
+        }
+    } else {
+        this.setDanger("Field is required")
+        this.validated = false
+    }
+}
+
+inputCountry.validate = inputCountry.validateEmpty
+
+
+export const inputFields = [inputStreet, inputNumber, inputCity, inputCountry]
