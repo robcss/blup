e09fe29e4e302e018c0d129a0867521234161751
robcass <77405130+robcss@users.noreply.github.com>
2021-05-12 12:59:48 +0200

    add reportCount tag ajax

--- public/js/reports/index.js
+++ public/js/reports/index.js
@@ -8,6 +8,8 @@ const postReportButton = document.getElementById("submitButton")
 const modalClose = document.getElementById("modal_reports").querySelector(".modal-close")
 const reportsTab = document.getElementById("tab_reports").querySelector("a")
 
+
+
 //post report events
 postReportButton.addEventListener("click", event => {
     event.preventDefault()
@@ -25,11 +27,15 @@ postReportButton.addEventListener("mouseup", async event => {
 
         await postReport(reportForm, reports)
 
+        updateReportCount(1)
+
         reportForm.reset()
 
         modalClose.click()
 
         reportsTab.click()
+
+
 
     } catch (e) {
         if (e.message === "401") {
@@ -54,6 +60,9 @@ reports.addEventListener("click", async event => {
 
         try {
             await resolveReport(resolveReportButton)
+
+            updateReportCount(-1)
+
         } catch (e) {
 
             if (e.message === "401") {
@@ -67,4 +76,31 @@ reports.addEventListener("click", async event => {
 
 })
 
+
+
+const updateReportCount = (inc) => {
+
+    const tag = document.getElementById("tag_reportCount")
+    const anchor = document.getElementById("tab_reports").querySelector("a")
+
+    if (!tag && inc < 1) return
+
+    if (!tag) {
+        const span = document.createElement("span")
+        span.innerText = inc
+        span.classList.add("ml-2", "tag", "is-warning", "is-rounded")
+        span.id = "tag_reportCount"
+        anchor.appendChild(span)
+        return
+    }
+
+    const reportCount = parseInt(tag.innerText) + inc
+
+    if (reportCount < 1) {
+        tag.remove()
+    } else {
+        tag.innerText = reportCount
+    }
+
+}
 
