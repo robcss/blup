b9ab46df7a4acc8a502966c07be816697dc65024
robcass <77405130+robcss@users.noreply.github.com>
2021-05-14 19:44:36 +0200

    refactor user middleware

--- routes/user.js
+++ routes/user.js
@@ -4,17 +4,17 @@ const passport = require('passport')
 
 const catchAsync = require("../utils/catchAsync")
 
-const { isLoggedIn } = require("../middleware")
+const isUserLoggedIn = require("../middleware/isUserLoggedIn")
 
 const users = require("../controllers/users-controller")
 
 router.route('/register')
-    .get(isLoggedIn({ isIn: "forbidLogin" }), users.renderRegisterForm)
-    .post(isLoggedIn({ isIn: "forbidLogin" }), catchAsync(users.register));
+    .get(isUserLoggedIn({ isIn: "forbidLogin" }), users.renderRegisterForm)
+    .post(isUserLoggedIn({ isIn: "forbidLogin" }), catchAsync(users.register));
 
 router.route('/login')
-    .get(isLoggedIn({ isIn: "forbidLogin" }), users.renderLoginForm)
-    .post(isLoggedIn({ isIn: "forbidLogin" }), passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+    .get(isUserLoggedIn({ isIn: "forbidLogin" }), users.renderLoginForm)
+    .post(isUserLoggedIn({ isIn: "forbidLogin" }), passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 router.get('/logout', users.logout)
 
