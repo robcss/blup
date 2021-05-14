a32085bcae8e9df49fe12f879fbf74496958f401
robcass <77405130+robcss@users.noreply.github.com>
2021-05-14 18:44:50 +0200

    delete commented code

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -38,34 +38,3 @@ module.exports.deleteComment = async (req, res) => {
     res.end()
 }
 
-
-// module.exports.createComment = async (req, res) => {
-
-//     const fountainId = req.params.id
-//     const commentBody = req.body
-
-//     const fountain = await Fountain.findById(fountainId)
-
-//     const newComment = new Comment({ body: commentBody })
-//     newComment.author = req.user._id;
-
-//     fountain.comments.push(newComment)
-
-//     await newComment.save()
-
-//     await fountain.save()
-
-//     const comment = await Comment.findById(newComment._id).populate('author')
-
-//     res.render("comments/showOne", { fountain, comment })
-// }
-
-// module.exports.deleteComment = async (req, res) => {
-//     const { id, commentId } = req.params
-
-//     await Fountain.findByIdAndUpdate(id, { $pull: { comments: commentId } })
-
-//     await Comment.findByIdAndDelete(commentId)
-
-//     res.end()
-// }
