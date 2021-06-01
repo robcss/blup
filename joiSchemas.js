96fb35748015120a285ca3af1eb764e3f2885fa9
robcass <77405130+robcss@users.noreply.github.com>
2021-06-01 18:45:03 +0200

    add sanitizeHtml joi extension

--- joiSchemas.js
+++ joiSchemas.js
@@ -1,14 +1,36 @@
-const Joi = require('joi');
+const BaseJoi = require('joi')
+const sanitizeHtml = require('sanitize-html')
 
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+})
+
+const Joi = BaseJoi.extend(extension)
 
 const addressSchema = Joi.object({
 
-    street: Joi.string().required(), //consider use of client side regex too
+    street: Joi.string().required().escapeHTML(), //consider use of client side regex too
     number: Joi.number().required().positive().integer(),
     postcode: Joi.number().positive().integer().empty(''),
-    city: Joi.string().required(),
-    state: Joi.string().empty(''),
-    country: Joi.string().required()
+    city: Joi.string().required().escapeHTML(),
+    state: Joi.string().empty('').escapeHTML(),
+    country: Joi.string().required().escapeHTML()
 
 }).required();
 
@@ -21,14 +43,14 @@ const fountainSchema = Joi.object({
 
 const commentSchema = Joi.object({
 
-    body: Joi.string().required()
+    body: Joi.string().required().escapeHTML()
 });
 
 
 const reportSchema = Joi.object({
 
-    title: Joi.string().required(),
-    description: Joi.string().required()
+    title: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML()
 });
 
 module.exports.addressSchema = addressSchema
