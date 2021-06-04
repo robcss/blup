67f2b896bc8e2575329e3bd5e84c6a7cda02890e
robcass <77405130+robcss@users.noreply.github.com>
2021-06-04 21:45:44 +0200

    add set DB_URL according to environment

--- config/index.js
+++ config/index.js
@@ -1,7 +1,5 @@
 require('dotenv').config()
 
-const DB_URL = 'mongodb://localhost:27017/fountain-finder'
-// const DB_URL = process.env.DB_URL
 
 module.exports = {
 
@@ -18,7 +16,7 @@ module.exports = {
 
     MAPBOX_TOKEN: process.env.MAPBOX_TOKEN,
 
-    DB_URL,
+    DB_URL: process.env.NODE_ENV === "production" ? process.env.DB_URL : 'mongodb://localhost:27017/fountain-finder',
 
     SESSION_SECRET: process.env.SESSION_SECRET,
     MONGOSTORE_SECRET: process.env.MONGOSTORE_SECRET
